22e3844d4b1e9860800482d771898afe24ae0414
jpais01 <jpais01@ucm.es>
2026-03-09 18:18:31 +0100

    base de datos local

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,9 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "n0A35DSbiMkvi1f9dCXS");
+define("DB_PASS", "");
 define("DB_NAME", "BistroFDI_G8");
 
 ?>
+
+<!-- n0A35DSbiMkvi1f9dCXS -->
