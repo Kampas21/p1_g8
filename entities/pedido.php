a6288d09ea4febe6aba5518fdcb9268fccd4a4f0
mancal02 <mancal02@gmail.com>
2026-03-11 10:51:00 +0100

    estructura de pedidods creada

---
+++ entities/pedido.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../includes/application.php';
+
+class Pedido
+{
+    
+}
--- includes/MySQL/02-tablas.sql
+++ includes/MySQL/02-tablas.sql
@@ -22,3 +22,46 @@ CREATE TABLE IF NOT EXISTS `productos` (
     `ofertado` BOOLEAN,
     FOREIGN KEY (`categoria_id`) REFERENCES `categorias`(`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `pedidos`(
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+
+    `numero_pedido` INT DEFAULT NULL,
+    `fecha_hora` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    `estado` ENUM('nuevo', 'recibido', 'en_preparacion', 'cocinando', 'listo_cocina', 'terminado', 'entregado', 'cancelado'),
+    `tipo` ENUM('local', 'llevar'), 
+    `metodo_pago` ENUM('tarjeta', 'camarero') DEFAULT NULL,
+    `usuario_id` INT NOT NULL,
+    `total` DECIMAL(10,2),
+    `cocinero_id` INT DEFAULT NULL,
+
+    -- Clave foránea hacia la tabla de usuarios para el cliente que hizo el pedido
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+
+    -- El número de pedido es único para un día concreto
+    UNIQUE (DATE(fecha_hora), numero_pedido),
+
+    -- Clave foránea hacia la tabla de usuarios para el cocinero asignado (se rellena cuando pasa a cocinando)
+    FOREIGN KEY (cocinero_id) REFERENCES usuarios(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS `productos_en_pedido` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+
+    `pedido_id` INT NOT NULL,
+    `producto_id` INT NOT NULL,
+
+    `cantidad` INT NOT NULL DEFAULT 1,
+    `precio_unitario` DECIMAL(10,2) NOT NULL,
+
+    `estado` ENUM('pendiente', 'preparado') DEFAULT 'pendiente',
+
+    -- para que si se borra un pedido o un producto, se borren también de esta tabla
+    FOREIGN KEY (`pedido_id`) REFERENCES `pedidos`(`id`),
+    FOREIGN KEY (`producto_id`) REFERENCES `productos`(`id`),
+
+    -- para que no se inserte el mismo producto dos veces en el mismo pedido, sino que incrementamos su cantidad
+    UNIQUE (pedido_id, producto_id)
+);
