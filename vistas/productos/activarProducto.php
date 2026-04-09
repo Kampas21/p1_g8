525b7a27a5522f1137c7a7b86787f22f5eb5e0a7
Roberto Bernabéu Leco <robernab@ucm.es>
2026-04-09 18:22:46 +0200

    hey

--- vistas/productos/activarProducto.php
+++ vistas/productos/activarProducto.php
@@ -1,20 +1,25 @@
 <?php
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/productoService.php';
 
+$user = current_user();
+
+// 🔒 Solo gerente
+if (!$user || !user_has_role($user, 'gerente')) {
+    die("Acceso denegado");
+}
+
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-$categoria_id = filter_input(INPUT_POST, 'categoria_id', FILTER_VALIDATE_INT);
 
-if (!$id) {
-    die("ID inválido");
+if ($id) {
+    ProductoService::activar($id);
 }
 
-ProductoService::activar($id);
-
-if ($categoria_id) {
-    header("Location: mostrarProductosCategoria.php?id=" . $categoria_id);
-    exit;
-}
-
-header("Location: ../categorias/categoriasList.php");
+// Redirigir
+header("Location: " . $_SERVER['HTTP_REFERER']);
 exit;
