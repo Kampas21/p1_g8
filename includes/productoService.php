204b3ec828487903f0275fee98b3dadaa1142208
Roberto Bernabéu Leco <robernab@ucm.es>
2026-04-09 17:59:29 +0200

    hey

--- includes/productoService.php
+++ includes/productoService.php
@@ -93,7 +93,6 @@ class ProductoService {
     public static function create($nombre, $descripcion, $categoria_id, $precio, $iva) {
         global $conn;
 
-<<<<<<< HEAD
         $disponible = 1;
         $ofertado = 1;
 
@@ -146,46 +145,6 @@ class ProductoService {
     }
 
     public static function activar($id) {
-=======
-        $stmt = $conn->prepare("INSERT INTO productos (nombre, descripcion, categoria_id, precio_base, iva, ofertado) VALUES (?, ?, ?, ?, ?, 1)");
-        $stmt->bind_param("ssidd", $nombre, $descripcion, $categoria_id, $precio, $iva);
-
-        return $stmt->execute();
-    }
-
-    public static function update($id, $nombre, $descripcion, $categoria_id, $precio, $iva) {
-        global $conn;
-
-        $stmt = $conn->prepare("
-        UPDATE productos 
-        SET nombre = ?, descripcion = ?, categoria_id = ?, precio_base = ?, iva = ?
-        WHERE id = ?
-        ");
-
-        $stmt->bind_param("ssiddi", $nombre, $descripcion, $categoria_id, $precio, $iva, $id);
-
-        return $stmt->execute();
-    }
-
-    public static function activar($id) {
-        global $conn;
-
-        $stmt = $conn->prepare("UPDATE productos SET ofertado = 1 WHERE id = ?");       
-        $stmt->bind_param("i", $id);
-        return $stmt->execute();
-    }
-
-    public static function desactivar($id) {
-        global $conn;
-
-        $stmt = $conn->prepare("UPDATE productos SET ofertado = 0 WHERE id = ?");
-        $stmt->bind_param("i", $id);
-        return $stmt->execute();
-    }
-
-    public static function getProductosDeOferta($oferta_id)
-    {
->>>>>>> c55de23354cd414581ca0daa6ccf2323ee5ba1a6
         global $conn;
 
         $stmt = $conn->prepare("UPDATE productos SET ofertado = 1 WHERE id = ?");
@@ -196,7 +155,6 @@ class ProductoService {
         return $ok;
     }
 
-<<<<<<< HEAD
     public static function desactivar($id) {
         global $conn;
 
@@ -206,28 +164,5 @@ class ProductoService {
         $stmt->close();
 
         return $ok;
-=======
-        $producto = new Producto(
-            $fila['id'],
-            $fila['nombre'],
-            $fila['descripcion'],
-            $fila['categoria_id'],
-            $fila['precio_base'],
-            $fila['iva'],
-            $fila['disponible'],
-            $fila['ofertado']
-        );
-
-        $producto->cantidad = $fila['cantidad'];
-
-        $productos[] = $producto;
-        }
-
-        $result->free();
-        $stmt->close();
-
-        return $productos;
->>>>>>> c55de23354cd414581ca0daa6ccf2323ee5ba1a6
     }
-    
 }
