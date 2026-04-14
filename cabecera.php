65792d6d7ab93222eaf04cc39ca4eb1c1248a3b9
mancal02 <mancal02@gmail.com>
2026-04-14 20:00:52 +0200

    quitados algunos session start y sidebar der se ve en moviles y tablets

--- cabecera.php
+++ cabecera.php
@@ -1,9 +1,6 @@
 <?php
 declare(strict_types=1);
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 require_once __DIR__ . '/includes/auth.php';
 
--- sideBarIzq.php
+++ sideBarIzq.php
@@ -1,7 +1,4 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 $user = function_exists('current_user') ? current_user() : null;
 ?>
