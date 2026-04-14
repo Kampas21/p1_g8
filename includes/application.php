df30048972aa51c3b6455ff63bc4422da8c2118f
mancal02 <mancal02@gmail.com>
2026-04-14 18:11:24 +0200

    prueba 3

--- includes/application.php
+++ includes/application.php
@@ -5,15 +5,16 @@ require_once __DIR__ . '/config.php';
 $conn = crearConexion();
 
 
+
 function crearConexion() {
-
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-    if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
+    try {
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        return $conn;
+    } catch (Exception $e) {
+        // En local usaríamos throw $e o die, 
+        // pero imprimimos en pantalla para poder depurar en el VPS:
+        die("Error crítico conectando a la base de datos: " . $e->getMessage());
     }
-
-    return $conn;
 }
 
 ?>
