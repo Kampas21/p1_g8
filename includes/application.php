ddf3e83a262e88e41351b8f067c4ef71c5353642
mancal02 <mancal02@gmail.com>
2026-04-14 18:38:15 +0200

    prueba 4

--- includes/application.php
+++ includes/application.php
@@ -7,8 +7,13 @@ $conn = crearConexion();
 
 
 function crearConexion() {
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     try {
         $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($conn->connect_error) {
+            die("Error de conexión: " . $conn->connect_error);
+        }
+        $conn->set_charset("utf8mb4");
         return $conn;
     } catch (Exception $e) {
         // En local usaríamos throw $e o die, 
@@ -16,6 +21,3 @@ function crearConexion() {
         die("Error crítico conectando a la base de datos: " . $e->getMessage());
     }
 }
-
-?>
-
